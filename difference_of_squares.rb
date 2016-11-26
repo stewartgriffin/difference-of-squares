196bc8169b2cb0cf9f2ba2b6aa15b13ae0d1ebf5
Ziajko <tomasz.ziajko@avl.com>
2016-11-26 11:19:19 +0100

    Pass: test_sum_of_squares_5, test_sum_of_squares_10, test_sum_of_squares_100

--- difference_of_squares.rb
+++ difference_of_squares.rb
@@ -6,7 +6,14 @@ class Squares
   def initialize(number)
     @squares_to_calculate = Range.new(0,number)
   end
+  
   def square_of_sum
     @squares_to_calculate.reduce(:+)**2
+  end
+  
+  def sum_of_squares
+    @squares_to_calculate.inject do |sum_of_sq, n|
+      sum_of_sq += n**2
+    end
   end
 end
